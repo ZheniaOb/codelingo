50fa6ee15ec40e71c465650b0643525e82bc34d9
Karyna Trush <ktrush60@gmail.com>
2025-12-05 03:18:02 +0100

    fix PythonPage.jsx

--- frontend/src/components/Courses/Python/PythonPage.jsx
+++ frontend/src/components/Courses/Python/PythonPage.jsx
@@ -1,8 +1,4 @@
-
 import React, { useState, useEffect } from "react";
-
-import React, { useState } from "react";
-
 import { useNavigate } from "react-router-dom";
 import Footer from "../../BasicSiteView/Footer/Footer";
 import "./PythonPage.css";
